f596f216d7d215e79bd9a423042f02a1286bb659
Paul Laffitte <paul.laffitte@enix.fr>
2021-06-07 13:22:41 +0200

    chore(node): chroot dothill-node in /host

--- cmd/node/node.go
+++ cmd/node/node.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"syscall"
 
 	"github.com/enix/dothill-csi/pkg/common"
 	"github.com/enix/dothill-csi/pkg/node"
@@ -11,11 +12,19 @@ import (
 
 var kubeletPath = flag.String("kubeletpath", "/var/lib/kubelet", "Kubelet path")
 var bind = flag.String("bind", fmt.Sprintf("unix:///var/lib/kubelet/plugins/%s/csi.sock", common.PluginName), "RPC bind URI (can be a UNIX socket path or any URI)")
+var chroot = flag.String("chroot", "", "Chroot into a directory at startup (used when running in a container)")
 
 func main() {
 	klog.InitFlags(nil)
 	flag.Set("logtostderr", "true")
 	flag.Parse()
+
+	if *chroot != "" {
+		if err := syscall.Chroot(*chroot); err != nil {
+			panic(err)
+		}
+	}
+
 	klog.Infof("starting dothill storage node plugin %s", common.Version)
 	node.New(*kubeletPath).Start(*bind)
 }
